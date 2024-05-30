b52957dc3becc27778612b3d38155042093d7fae
hocinedev <hocinedev4@gmail.com>
2024-05-30 22:43:21 +0100

    some updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Merci pour votre message ! Je vous répondrai dans les plus brefs délais.');
